d968769dc008d955c997667e28604d587fa04901
Brett V. Forsgren <brett.forsgren@outlook.com>
2019-09-23 14:52:00 -0700

    add optional test that parses the entire OTP directory

---
+++ src/IxMilia.Erlang.Test/FactPathTestAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using Xunit;
+
+namespace IxMilia.Erlang.Test
+{
+    public class FactPathTestAttribute : FactAttribute
+    {
+        public FactPathTestAttribute(string path)
+        {
+            if (!Directory.Exists(path))
+            {
+                Skip = $"Path does not exist: '{path}'";
+            }
+        }
+    }
+}
---
+++ src/IxMilia.Erlang.Test/IntegrationTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using IxMilia.Erlang.Syntax;
+using IxMilia.Erlang.Tokens;
+
+namespace IxMilia.Erlang.Test
+{
+    public class IntegrationTests
+    {
+        private const string OTPSources = @"C:\Program Files\erl10.5\lib\stdlib-3.10\src";
+
+        [FactPathTest(OTPSources)]
+        public void ParseOtp()
+        {
+            foreach (var file in Directory.EnumerateFiles(OTPSources, "*.erl"))
+            {
+                Console.WriteLine($"Parsing {file}.");
+                var code = File.ReadAllText(file);
+                var expression = ErlangSyntaxNode.ParseExpression(new TokenBuffer(ErlangToken.Tokenize(new TextBuffer(code))));
+                var compiledExpr = ErlangExpression.Compile(expression);
+            }
+        }
+    }
+}
